7dce5254c3739a82c5a2c607dca6d43b959b6d68
mdnuzen <mdnuzen@gmail.com>
2023-10-21 14:52:34 -0700

    Revert "Update PlayerMovement.cs"
    This reverts commit 39dcef0c6a8838ed26e2cdcefc6e515615889748.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,57 +3,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    public CharacterController controller;
-    private float grav = -9.81f;
-    private bool isGrounded;
-    private Vector3 playerVelocity;
-    private float jumpHeight = 1.0f;
-    private float playerSpeed = 2.0f;
-    public float rotationSpeed = 1.0f;
-    public string playerRotation = "left";
-    public SpriteRenderer m_SpriteRenderer;
-
-    void Start()
-    {
-        controller = GetComponent<CharacterController>();
-        m_SpriteRenderer = GetComponent<SpriteRenderer>();
-    }
+    //public float rotationSpeed;
 
     void Update()
     {
-        isGrounded = controller.isGrounded;
-        if (isGrounded && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
-
-        if(Input.GetKey(KeyCode.Space) && isGrounded)
-        {
-            Debug.Log("Jump!");
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * grav);
-        }
-
         float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        float verticalInput = 0;
 
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        controller.Move(movementDirection * Time.deltaTime * playerSpeed);
         movementDirection.Normalize();
 
         transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
-        playerVelocity.y += grav * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
-
-        if((Input.GetKey(KeyCode.A) ||  Input.GetKey(KeyCode.LeftArrow)) && playerRotation == "right")
+        
+        if (movementDirection != Vector3.zero)
         {
-            m_SpriteRenderer.flipX = true;
-            playerRotation = "left";
-        }
-
-        if((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && playerRotation == "left")
-        {
-            m_SpriteRenderer.flipX = false;
-            playerRotation = "right";
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);            
         }
     }
 }
